0b23f0e1f7f026f6b367403ab0fd8c3c5836c92b
Firdaus Bagus <120537868+FirdausBagus02@users.noreply.github.com>
2023-08-30 17:15:49 +0700

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,1 +1,20 @@
 
+num = 1
+
+while num <= 10:
+    if num > 1:
+        is_prime = True
+        i = 2
+        while i <= num ** 0.5:
+            if num % i == 0:
+                is_prime = False
+                break
+            i += 1
+        if is_prime:
+            print(num, "merupakan bilangan prima")
+        else:
+            print(num, "bukan merupakan bilangan prima")
+    else:
+        print(num, "bukan merupakan bilangan prima")
+    
+    num += 1
